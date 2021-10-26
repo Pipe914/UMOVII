a53de315f3ece5a77a8564a2dc54b44da0bbe300
KrLits <>
2021-10-25 22:29:17 -0500

    eso

--- pages/register/register.js
+++ pages/register/register.js
@@ -36,15 +36,22 @@ Page({
 formSubmit: function(e) {
     console.log('form has a submit event, carrying data ', e.detail.value)
     my.alert({title: e.detail.value.mail})
-    if (e.detail.value.pass1 == e.detail.value.pass2){
+    if (e.detail.value.pass1 == e.detail.value.pass2 && !(e.detail.value.mail=="")){
       my.alert({
         title: "Usuario registrado",
         content: "Usuario registrado exitosamente",
         buttonText: "Ok",
       })
       my.navigateTo({
-        url: '...'
+        url: '../index/index'
+
       })  
+    }else{
+      my.alert({
+        title: "Usuario no registrado",
+        content: "No se ha podido registrar el usuario. Intente de nuevo",
+        buttonText: "Ok",
+      })
     }
   }
 });
