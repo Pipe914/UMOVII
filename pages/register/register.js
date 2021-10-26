0457e32971e37ee3d72f20ddeb5900f264fc9f61
KrLits <>
2021-10-25 22:50:05 -0500

    Probando create map context

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,42 +1,8 @@
 Page({
   data: {},
   onLoad() { },
-  Registrarse(a,e) {
-
-
-    // my.showLoading({
-    //   content: 'Cargando...',
-    //   delay: 1000
-    // });
-    my.alert({title: e.detail.value});
-    if (a.detail.value["mail"] === a.detail.value["pass1"]) {
-      my.alert({
-        title: 'Registro Exitoso',
-        content: 'Se ha registrado con exito el usuario',
-        buttonText: 'Ok',
-        // success: () => {
-        //   my.alert({
-        //     title: 'nvm',
-        //   });
-        // },
-      });
-    } else {
-      my.alert({
-        title: 'Registro no exitoso',
-        content: 'Intente de nuevo mas tarde',
-        buttonText: 'Ok',
-        
-       
-      });
-    
-
-  }
-  // my.hideLoading();
-},
-formSubmit: function(e) {
-    console.log('form has a submit event, carrying data ', e.detail.value)
-    my.alert({title: e.detail.value.mail})
-    if (e.detail.value.pass1 == e.detail.value.pass2 && !(e.detail.value.mail=="")){
+  formSubmit: function (e) {
+    if (e.detail.value.pass1 == e.detail.value.pass2 && !(e.detail.value.mail == "")) {
       my.alert({
         title: "Usuario registrado",
         content: "Usuario registrado exitosamente",
@@ -44,9 +10,8 @@ formSubmit: function(e) {
       })
       my.navigateTo({
         url: '../index/index'
-
-      })  
-    }else{
+      })
+    } else {
       my.alert({
         title: "Usuario no registrado",
         content: "No se ha podido registrar el usuario. Intente de nuevo",
