9ff842173c334083ef8d6ed67933dd438c9ea9cf
KrLits <>
2021-10-26 11:56:43 -0500

    Verificaciones de js hechas para las pantallas

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,22 @@
 Page({
   data: {},
   onLoad() {},
+  formSubmit: function(e){
+    if(e.detail.value.mail.contains("@unisabana.edu.co") && e.detail.value.pass != ""){
+      my.alert({
+        title: "Inicio de sesion exitoso",
+        content: "Se ha iniciado sesion exitosamente",
+        buttonText: "Ok",
+      });
+      my.navigateTo({
+        url: '../index/index'
+      });
+    }else{
+      my.alert({
+        title: "Usuario no encontrado",
+        content: "No existe el usuario",
+        buttonText: "Ok",
+      });
+    }
+  },
 });
