8f8b45ecf0b67eed27b40dd0d1de52c4ec9c9b57
KrLits <>
2021-10-25 20:20:53 -0500

    no c

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,4 +1,35 @@
 Page({
   data: {},
-  onLoad() {},
+  onLoad() { },
+  Registrarse(a) {
+
+
+    my.showLoading({
+      content: 'Cargando...',
+      delay: 1000
+    });
+    if (a.detail.value.contra === a.detail.value.contra2) {
+      my.alert({
+        title: 'Registro Exitoso',
+        content: 'Se ha registrado con exito el usuario',
+        buttonText: 'Ok',
+        // success: () => {
+        //   my.alert({
+        //     title: 'nvm',
+        //   });
+        // },
+      });
+    } else {
+      my.alert({
+        title: 'Registro no exitoso',
+        content: 'Intente de nuevo mas tarde',
+        buttonText: 'Ok',
+        
+       
+      });
+    
+
+  }
+  my.hideLoading();
+}
 });
