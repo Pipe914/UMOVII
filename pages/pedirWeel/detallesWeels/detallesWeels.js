8b3291b2d1ca7cdd665c39b79e216cda2878a029
KrLits <>
2021-10-31 20:36:16 -0500

    x2

--- pages/pedirWeel/detallesWeels/detallesWeels.js
+++ pages/pedirWeel/detallesWeels/detallesWeels.js
@@ -1,44 +1,44 @@
 const app = getApp()
 Page({
   data: {},
-  onLoad() {},
-  formSubmit: function(e){
+  onLoad() { },
+  formSubmit: function (e) {
     let modelo = e.detail.value.modelo;
     let cupos = e.detail.value.cupos;
     let precio = e.detail.value.precio;
     let hora = e.detail.value.hora;
 
-    console.log(modelo+"este es el modelo")
+    console.log(modelo + "este es el modelo")
 
-    if(modelo !="" && cupos!="" && precio!="" && hora!=""){
-      if(app.globalData.dataUserRegister.tipoUser=="Conductor"){
+    if (modelo != "" && cupos != "" && precio != "" && hora != "") {
+      if (app.globalData.dataUserRegister.tipoUser == "Conductor") {
         console.log(app.globalData.dataUserRegister.carro.modelo)
-        console.log(app.globalData.dataUserRegister.carro.modelo==modelo)
-        if(app.globalData.dataUserRegister.carro.modelo==modelo){
-          app.globalData.dataUserRegister.viaje.precio = precio;
-          app.globalData.dataUserRegister.viaje.hora = hora;
-          my.alert({
-        title: "Viaje Registrado!",
-        button: "Ok"
-      });
-      my.redirectTo({
-        url: '../../splash/splash',
-      });
-        }else{
-          my.alert({
-        title: "Error",
-        content: "No esta registrado ese modelo",
-        button: "Ok"
-      });
-        }
-      }else{
+        console.log(app.globalData.dataUserRegister.carro.modelo == modelo)
+        // if(app.globalData.dataUserRegister.carro.modelo==modelo){
+        app.globalData.dataUserRegister.viaje.precio = precio;
+        app.globalData.dataUserRegister.viaje.hora = hora;
         my.alert({
-        title: "Error",
-        content: "No hay un conductor registrado!",
-        button: "Ok"
-      });
+          title: "Viaje Registrado!",
+          button: "Ok"
+        });
+        my.redirectTo({
+          url: '../../splash/splash',
+        });
+        //   }else{
+        //     my.alert({
+        //   title: "Error",
+        //   content: "No esta registrado ese modelo",
+        //   button: "Ok"
+        // });
+        //   }
+      } else {
+        my.alert({
+          title: "Error",
+          content: "No hay un conductor registrado!",
+          button: "Ok"
+        });
       }
-    }else{
+    } else {
       my.alert({
         title: "Campos incompletos",
         content: "Se deben llenar todos los campos",
